60a8debd75a9a52bab0ba2b35897f88ae2300ca0
demirmeltem <demirmeltem50@gmail.com>
2017-05-29 14:56:24 +0300

    last modified

--- statictical-rapor1/deneme.R
+++
@@ -1,6 +0,0 @@
-büyük <- subset(air.crash, Accident.Year>2000, select = c(Number.of.Dead, Accident.Year))
-büyük <- aggregate(data = büyük, Number.of.Dead ~ Accident.Year, FUN=sum)
-
-kücük <- subset(air.crash, Accident.Year<2000, select = c(Number.of.Dead, Accident.Year))
-kücük <- aggregate(data = kücük, Number.of.Dead ~ Accident.Year, FUN=sum)
-kücük <- tail(kücük, 9)
